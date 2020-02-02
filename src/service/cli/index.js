5752247c55df6c068dc16fc70ca4191333a696b4
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-02 19:34:43 +0500

    Добавляет код для команд generate и version

---
+++ src/service/cli/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const {cli} = require(`./src/service.cli`);
---
+++ src/service/cli/version.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const packageJsonFile = require(`../../../package.json`);
+
+module.export = {
+  name: `--version`,
+  run() {
+    const version = packageJsonFile.version;
+    console.info(version);
+  }
+};
