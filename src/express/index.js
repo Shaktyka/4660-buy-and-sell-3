3dd98756fa2e4932cc7066a634195c3fecda5508
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-22 16:03:51 +0500

    Добавляет начальную отрисовку тестового шаблона Pug

--- src/express/index.js
+++ src/express/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 const loginRouter = require(`./routes/login`);
 const registerRouter = require(`./routes/register`);
@@ -10,13 +11,18 @@ const offersRouter = require(`./routes/offers`);
 const WORK_PORT = 8080;
 const app = express();
 
+app.set(`views`, path.join(__dirname, `./templates`));
+app.set(`view engine`, `pug`);
+
 app.use(`/login`, loginRouter);
 app.use(`/register`, registerRouter);
 app.use(`/my`, myRouter);
 app.use(`/search`, searchRouter);
 app.use(`/offers`, offersRouter);
 
-app.get(`/`, (req, res) => res.send(`/`));
+app.get(`/`, (req, res) => {
+  res.render(`index`);
+});
 
 app.listen(WORK_PORT, () =>
   console.log(`Сервер запущен на порту: ${WORK_PORT}`));
