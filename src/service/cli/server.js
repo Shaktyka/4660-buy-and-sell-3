f08a7a7c04fdcdbce8df1702a183f61978e39f73
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-29 17:54:42 +0500

    Рефакторинг получения и отправки моковых данных

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,12 +2,13 @@
 
 const express = require(`express`);
 const fs = require(`fs`).promises;
+const log = require(`../../paint-log.js`).log;
 
 const DEFAULT_PORT = 3000;
 const MOCKS_FILE = `mocks.json`;
 const NOT_FOUND_MESSAGE = `Файл ${MOCKS_FILE} не найден`;
 const EMPTY_FILE_MESSAGE = `Файл ${MOCKS_FILE} пустой`;
-const DATA_SENT_MESSAGE = `Данные из файла ${MOCKS_FILE} отправлены`;
+const DATA_SENT_MESSAGE = `Данные отправлены`;
 
 const ServerLogText = {
   ERROR: `Ошибка при создании сервера`,
@@ -21,29 +22,37 @@ const router = new Router();
 app.use(express.json());
 app.use(`/offers`, router);
 
-const checkMockFile = async (res) => {
-  await fs.readFile(MOCKS_FILE, `utf8`)
-    .then((data) => {
-      if (data === ``) {
-        res.send([]);
-        console.log(EMPTY_FILE_MESSAGE);
-      } else {
-        res.json(data);
-        console.log(DATA_SENT_MESSAGE);
-      }
-    })
-    .catch((err) => {
-      if (err.code === `ENOENT`) {
-        console.error(NOT_FOUND_MESSAGE);
-      } else {
-        console.error(err);
-      }
-      res.send([]);
-    });
+const readMockData = async () => {
+  let data = [];
+
+  try {
+    data = await fs.readFile(MOCKS_FILE, `utf8`);
+    if (data === ``) {
+      data = [];
+      log(EMPTY_FILE_MESSAGE, `error`, `error`);
+    }
+  } catch (err) {
+    if (err.code === `ENOENT`) {
+      log(NOT_FOUND_MESSAGE, `error`, `error`);
+    } else {
+      log(err, `error`, `error`);
+    }
+  }
+
+  return data;
 };
 
 router.use(`/`, (req, res) => {
-  checkMockFile(res);
+  const result = readMockData();
+
+  if (result instanceof Promise) {
+    result
+      .then((data) => {
+        res.json(data);
+        log(DATA_SENT_MESSAGE, `log`, `success`);
+      })
+      .catch((err) => log(err, `error`, `error`));
+  }
 });
 
 module.exports = {
