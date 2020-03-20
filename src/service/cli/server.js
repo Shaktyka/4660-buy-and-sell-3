2e1632d74bda9739bd6ba932a71302fb958b289a
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-03-20 10:40:31 +0500

    Добавляет обработчики ошибок 404 и 500

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -55,6 +55,15 @@ router.use(`/`, (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  res.status(404).send('404: страница не найдена');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('500: на сервере что-то пошло не так...');
+});
+
 module.exports = {
   name: `--server`,
   run(args) {
