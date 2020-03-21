5f4000623b12d6b2eb6e92b4f506627d2cbdb78d
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-03-21 18:49:16 +0500

    Добавляет поиск по заголовкам

--- src/service/cli/routes/search.js
+++ src/service/cli/routes/search.js
@@ -1,20 +1,45 @@
 'use strict';
 
-const log = require(`../../../paint-log.js`).log;
 const {Router} = require(`express`);
 const searchRouter = new Router();
 const readFileData = require(`../../../utils.js`).readFileData;
 
-const DATA_SENT_MESSAGE = `Данные отправлены`;
+const MOCKS_FILE = `mocks.json`;
+// const DATA_SENT_MESSAGE = `Данные отправлены`;
+// const log = require(`../../../paint-log.js`).log;
 
 // Модель
-// Надо?
+const search = {
+  getList: async () => {
+    const offersList = await readFileData(MOCKS_FILE);
+    return offersList;
+  },
+
+  getMatches: async (text) => {
+    const offers = await search.getList();
+    const parsedOffers = JSON.parse(offers);
+    const matches = [];
+    const regExp = new RegExp(text, `gi`);
+
+    parsedOffers.filter((offer) => {
+      if (offer.title.match(regExp)) {
+        matches.push(offer);
+      }
+    });
+
+    return matches;
+  }
+};
 
 // Поиск объяаления по наменованию
 searchRouter.get(`/`, async (req, res) => {
-  // Получить строку запроса
+  const queryString = req.query.query.trim();
+  if (queryString.length === 0) {
+    res.sendStatus(404);
+  }
 
-  res.send(`/search`);
+  const result = await search.getMatches(queryString);
+  res.json(result);
 });
 
 module.exports = searchRouter;
