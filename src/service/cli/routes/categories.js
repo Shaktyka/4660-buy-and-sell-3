1b73f3a5a2aa0ec2c84f20946f187f2bc58b3980
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-03-21 14:38:16 +0500

    Добавляет вывод списка категорий

--- src/service/cli/routes/categories.js
+++ src/service/cli/routes/categories.js
@@ -1,10 +1,52 @@
 'use strict';
 
+const fs = require(`fs`).promises;
+const log = require(`../../../paint-log.js`).log;
 const {Router} = require(`express`);
 const categoriesRouter = new Router();
+const CATEGORIES_FILE = `data/categories.txt`;
+const NOT_FOUND_MESSAGE = `Файл ${CATEGORIES_FILE} не найден`;
+const EMPTY_FILE_MESSAGE = `Файл ${CATEGORIES_FILE} пустой`;
+const DATA_SENT_MESSAGE = `Данные отправлены`;
 
-categoriesRouter.get(`/`, (req, res) => {
-  res.send(`/categories`);
+const readFileData = (fileName) => {
+  let data = [];
+
+  try {
+    data = fs.readFile(fileName, `utf8`);
+    if (data === ``) {
+      data = [];
+      log(EMPTY_FILE_MESSAGE, `error`, `error`);
+    }
+  } catch (err) {
+    if (err.code === `ENOENT`) {
+      log(NOT_FOUND_MESSAGE, `error`, `error`);
+    } else {
+      log(err, `error`, `error`);
+    }
+  }
+
+  return data;
+};
+
+// Модель категорий
+const category = {
+  get: async () => {
+    let categories = await readFileData(CATEGORIES_FILE);
+    categories = categories.split(`\n`).filter((it) => it.length > 0);
+    return categories;
+  }
+};
+
+// Отдаёт все категории
+categoriesRouter.get(`/`, async (req, res) => {
+  try {
+    let response = await category.get();
+    res.json(response);
+    log(DATA_SENT_MESSAGE, `log`, `success`);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = categoriesRouter;
--- src/service/cli/routes/offers.js
+++ src/service/cli/routes/offers.js
@@ -4,17 +4,18 @@ const fs = require(`fs`).promises;
 const log = require(`../../../paint-log.js`).log;
 const {Router} = require(`express`);
 const offersRouter = new Router();
+// const Offer = require(`../models/offer.js`);
 
 const MOCKS_FILE = `mocks.json`;
 const NOT_FOUND_MESSAGE = `Файл ${MOCKS_FILE} не найден`;
 const EMPTY_FILE_MESSAGE = `Файл ${MOCKS_FILE} пустой`;
-const DATA_SENT_MESSAGE = `Данные отправлены`;
+// const DATA_SENT_MESSAGE = `Данные отправлены`;
 
-const readMockData = async () => {
+const readFileData = (fileName) => {
   let data = [];
 
   try {
-    data = await fs.readFile(MOCKS_FILE, `utf8`);
+    data = fs.readFile(fileName, `utf8`);
     if (data === ``) {
       data = [];
       log(EMPTY_FILE_MESSAGE, `error`, `error`);
@@ -30,25 +31,93 @@ const readMockData = async () => {
   return data;
 };
 
-offersRouter.use(`/`, (req, res) => {
-  const result = readMockData();
+// МОДЕЛЬ
 
-  if (result instanceof Promise) {
-    result
-      .then((data) => {
-        res.json(data);
-        log(DATA_SENT_MESSAGE, `log`, `success`);
-      })
-      .catch((err) => log(err, `error`, `error`));
+// Объект offers
+const offers = {
+  getList: async () => {
+    const offersList = await readFileData(MOCKS_FILE);
+    return offersList;
+  },
+
+  getOffer: (id) => {
+    let offer = {};
+    const offersList = offers.getList();
+
+    offersList.then((list) => {
+      const parsedList = JSON.parse(list);
+      for (let element of parsedList) {
+        if (element.id === id) {
+          offer = element;
+        } else {
+          break;
+        }
+      }
+      // console.log(1, offer);
+      return offer;
+    })
+    .catch((err) => log(err, `error`, `error`));
+    // return offer;
+  },
+
+  getOfferComments: (id) => {
+    let comments = [];
+    const offer = offers.getOffer(id);
+    console.log(offer);
+
+    return comments;
   }
+};
+
+
+// РОУТЫ
+
+offersRouter.get(`/`, async (req, res) => {
+  const result = await offers.getList();
+  res.json(result);
 });
 
-// offersRouter.get(`/:offerId`, (req, res) => {
-//   res.render(`/:offerId`);
-// });
+offersRouter.get(`/:offerId`, async (req, res) => {
 
-// offersRouter.get(`/:offerId/comments`, (req, res) => {
-//   res.render(`/:offerId/comments`);
-// });
+  const offerId = req.params.offerId.trim();
+  if (offerId.length === 0) {
+    res.sendStatus(404);
+    return;
+  }
+
+  // const result = await offers.getOffer(offerId)
+  // .then((data) => {
+  //   console.log(3, data);
+  // });
+
+  // res.json(result);
+});
+
+// Получить комментарии выбранного объявления
+offersRouter.get(`/:offerId/comments`, async (req, res) => {
+  const offerId = req.params.offerId.trim();
+  if (offerId.length === 0) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const result = await offers.getOfferComments(offerId);
+  res.json(result);
+});
+
+// Создаёт новое объявление
+offersRouter.post(`/`, (req, res) => {
+  res.send(`new offer`);
+});
+
+// Редактирует определённое объявление
+offersRouter.put(`/:offerId`, (req, res) => {
+  res.send(`update new offer`);
+});
+
+// Удаляет определённое объявление
+offersRouter.delete(`/:offerId`, (req, res) => {
+  res.send(`delete new offer`);
+});
 
 module.exports = offersRouter;
